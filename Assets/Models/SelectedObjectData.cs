5bd198da0a212fb9cad98e775d1f0e77990f49b0
Manan Chichra <133377496+mananchichra@users.noreply.github.com>
2026-02-22 04:11:58 +0530

    unity models

---
+++ Assets/Models/SelectedObjectData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class SelectedObjectData
+{
+    public string name;
+    public string tag;
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale;
+}
